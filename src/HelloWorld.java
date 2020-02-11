1d5b601b3a6acdbdc1942e951d2d6cd082a8108e
Glenn <gwallacec2@gmail.com>
2020-02-10 21:30:37 -0500

    test

---
+++ src/HelloWorld.java
@@ -0,0 +1,13 @@
+/*
+ * HelloWorld
+ * Program Description -
+ * Written by Glenn Wallace
+ * 2/10/2020
+ * JDK 1.8
+ */
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello World. "
+                +"\nGlenn Wallace");
+    }
+}
